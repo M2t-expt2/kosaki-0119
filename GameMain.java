4e16b03573a43aaecfdc347b0f5814f858c35936
bot-expt2 <156294560+bot-expt2@users.noreply.github.com>
2024-01-19 13:44:11 +0900

    GCBot:GameMain.javaを編集

--- GameMain.java
+++ GameMain.java
@@ -4,11 +4,20 @@ public class GameMain {
     // 課題User-1 文章の表示
     System.out.println("1:グー 2:チョキ 3:パー");
     System.out.println("じゃんけん...");
+
     // 課題Bot-1 Playerの手の決定とその表示
     // HandクラスのgetHandNameメソッドを使ってPlayerの手を表示する
+    if (args.length == 1) {
+      playerHand = Integer.parseInt(args[0]);
+    } else {
+      System.out.println("コマンドライン引数でプレイヤーの手を決定して下さい");
+      return;
+    }
+    System.out.println("プレイヤー : " + Hand.getHandName(playerHand));
 
     // 課題User-2 Computerの手の決定とその表示
     // ComputerクラスのdecidesComputerHandメソッドとHandクラスのgetHandNameメソッドを使ってPlayerの手を表示する
+
 
     // 課題Bot-2 勝敗判定とその表示
     // 課題Bot-1,User-2において実装したPlayerおよびComputerの手の表示はVictoryOrDefeatクラスに移動する
